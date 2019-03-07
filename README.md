9221d79c162bf2742a120de01019a8f6a1480773
skychen <dzhchxk@126.com>
2019-03-07 16:06:44 +0800

    add

---
+++ README.md
@@ -0,0 +1,152 @@
+# Notice
+Note that this code will be updated significantly in the near future 
+# Action Recognition Zoo
+Codes for popular action recognition models, written based on pytorch, verified on the [something-something](https://www.twentybn.com/datasets/something-something) dataset. This code is built on top of the [TRN-pytorch](https://github.com/metalbubble/TRN-pytorch).
+
+**Note** The main purpose of this repositoriy is to go through several methods and get familiar with their pipelines.
+
+**Note**: always use git clone --recursive https://github.com/coderSkyChen/Action_Recognition_Zoo to clone this project Otherwise you will not be able to use the inception series CNN architecture.
+
+## Depencies
+- Opencv-2.4.13 or some greater version that has tvl1 api for the computing of optical flow.
+- Pytorch-0.2.0_3
+- Tensorflow-1.3.1，this is only for the using of tensorboard, it's ok without this, but you need to comment the corresponding codes.
+
+## Data preparation
+### Dataset
+- **Download** the [something-something](https://www.twentybn.com/datasets/something-something) dataset. Decompress them into some folder.
+- Note that this dataset contains 108,499 videos and each video is presented in JPG images. The JPG images were extracted from the orginal videos at 12 frames per seconds.
+- The temporal evolution in videos is important for this dataset, so it's hard for some classic models which pay attention to short motion such as: Two-Stream Convolutional Networks for Action Recognition in Videos, NIPS 2014.
+### Prepare optical flow using Opencv
+Note that optical flow is an important modal feature in two-stream series methods, which contains the motion information of videos.
+
+Since there only rgb frames in the official dataset, we need compute optical flow by ourselves.
+
+I apply a TV-L1 optical flow algorithm, pixel values are truncated to the range \[-20, 20\], then rescaled between 0 and 255, each optical flow has two channels representing horizontal and vertical components. Note that the fps in original dataset is 12, which is too fast for optical flow computing in practice, so i sample frame at 6fps.
+
+- The command to compute optical flow:
+```
+cd optical_flow
+make bin                          #for cpu
+make -f gpu_makefile gpu_bin      #for gpu
+
+./bin          #for cpu
+./gpu_bin      #for gpu
+```
+Before using the code you should modify the path in main.cpp or gpu_main.cpp.
+
+### Generate the meta files
+```
+python process_dataset.py
+```
+
+# Models
+Before using the code you should modify the path as your own. The test time for one video is measured on one K80.
+## Two stream action recognition
+**Main Reference Paper**: [Two-stream convolutional networks for action recognition in videos](http://papers.nips.cc/paper/5353-two-stream-convolutional)
+
+![tsimage](https://github.com/coderSkyChen/Action_Recognition_Zoo/raw/master/Images_for_readme/two-stream.png)
+
+- Base CNN: BN-Inception pretrained on ImageNet.
+- Partical BN and cross-modality tricks have been used in the code.
+- Spatial stream: it's input is single rgb frame.
+- Temporal stream: it's input is stacked optical flows.
+### Training
+- Spatial CNN: A single rgb frame is randomly selected for a video, which equals to image classification，input channel is 3.
+- Temporal CNN: 5 consequent stacked optical flows are selected for a video, input channel is 5*2(2 channels:x and y).
+
+- The command to train models:
+```
+train for spatial stream:
+python main.py TwoStream RGB two-stream-rgb --arch BNInception --batch_size 256 --lr 0.002
+train for temporal stream:
+python main.py TwoStream Flow two-stream-flow --arch BNInception --batch_size 256 --lr 0.0005
+```
+### Testing on validation set
+At test time, given a video, i sample a fixed number of frames (25 for spatial stream and 8 for temporal stream in my experiments) with equal temporal spacing between them. From each of the frames i then obtain 10 ConvNet
+inputs by cropping and flipping four corners and the center of the frame. The class scores for the
+whole video are then obtained by averaging the scores across the sampled frames and crops therein.
+
+- The command to test models:
+```
+test for spatial stream:
+python test_models.py --model TwoStream --modality RGB --weights TwoStream_RGB_BNInception_best.pth.tar --train_id two-stream-rgb --save_scores rgb.npz --arch BNInception --test_segments 25
+
+test for temporal stream；
+python test_models.py --model TwoStream --modality Flow --weights TwoStream_Flow_BNInception_best.pth.tar --train_id two-stream-flow --save_scores flow.npz --arch BNInception --test_segments 25
+
+```
+After running the test code, we get the precision scores on validation set and the probability for all class is saved in npz files which is useful in late fusion.
+
+```
+fusion: combine spatial stream and temporal stream results.
+python average_scores.py
+```
+
+## Temporal Segment Networks
+**Main Reference Paper**: [Temporal Segment Networks: Towards Good Practices for Deep Action Recognition](https://arxiv.org/abs/1611.05267)
+
+![tsnimage](https://github.com/coderSkyChen/Action_Recognition_Zoo/raw/master/Images_for_readme/tsn.png)
+
+
+- Base CNN: BN-Inception pretrained on ImageNet.
+- Partical BN and cross-modality tricks have been used in the code.
+- Spatial stream: it's input is k rgb frames, k is the segment number.
+- Temporal stream: it's input is k stacked optical flows.
+- The consensus function i've implemented is average function.
+
+### Training
+
+```
+train spatial stream:
+python main.py TSN RGB tsn-rgb --arch BNInception --batch_size 128 --lr 0.001 --num_segments 3
+
+train temporal stream:
+python main.py TSN Flow tsn-flow --arch BNInception --batch_size 128 --lr 0.0007 --num_segments 3
+```
+### Testing on validation set
+Note that in testing phrase the k equals 1 according to the paper and it's offical code. So the segment mechanism is only used in training phrase.
+```
+test spatial stream:
+python test_models.py --model TSN --modality RGB --weights TSN_RGB_BNInception_best.pth.tar --train_id tsn-rgb --save_scores rgb.npz --arch BNInception --test_segments 25
+
+test temporal stream:
+python test_models.py --model TSN --modality Flow --weights TSN_Flow_BNInception_best.pth.tar --train_id tsn-flow --save_scores flow.npz --arch BNInception --test_segments 25
+
+fusion:
+python average_scores.py   # need modify the path to your own
+```
+
+## Pretrained-C3D :3D Convolutional Networks
+**Main Reference Paper**: [Learning Spatiotemporal Features with 3D Convolutional Networks](https://arxiv.org/abs/1412.0767)
+
+![c3d](https://github.com/coderSkyChen/Action_Recognition_Zoo/raw/master/Images_for_readme/c3d.png)
+
+- finetune the model pretrained on sports-1M, the pretrained model is upload to Baidu Cloud: [link](https://pan.baidu.com/s/1A-iAn4x45CHFgs7caOAFZw)
+
+### Training
+```
+python main.py C3D RGB c3d-rgb --arch BNInception --batch_size 32 --lr 0.0001 --num_segments 1 --lr_steps 2 5 10 20 --factor 0.5
+```
+
+### Testing
+```
+python test_models.py --model C3D --modality RGB --weights C3D_RGB_BNInception_best.pth.tar --train_id c3d-rgb --save_scores rgb.npz --test_segments 5 --test_crops 1
+```
+### Results on validation set
+- It seems like the C3D is faster than previous methods, but the input size for C3D is `112*112` vs `224*224` for Two-Stream models.
+- The result is not good. I've found that it's hard to traing 3D CNN on this difficult dataset. This is mainly due to the poor GPU which slows the training phrase, so it's hard to choose proper hyperparameters with my machine, but this code works and it'll give you a **quick start**.
+
+## I3D
+**Main Reference Paper**: [Quo Vadis, Action Recognition? A New Model and the Kinetics Dataset](https://arxiv.org/abs/1705.07750)
+
+![i3dimage](https://github.com/coderSkyChen/Action_Recognition_Zoo/raw/master/Images_for_readme/i3d.png)
+
+- The code for I3D model is based on [hassony2](https://github.com/hassony2/kinetics_i3d_pytorch)
+- Training is too slow to report the results on Something-Something, but this code is useful
+- Kinetics pretrained model is uploaded to Baidu Cloud: [link](https://pan.baidu.com/s/18pfAM2fYVsA6KxhX4A_pMQ)
+### Training
+```
+python main.py I3D RGB i3d-rgb --arch I3D --batch_size 32 --lr 0.002 --num_segments 1 --lr_steps 2 10 20 --factor 0.5
+```
+
---
+++ optical_flow/main.cpp
@@ -0,0 +1,309 @@
+#include<sys/types.h>
+#include<sys/stat.h>
+#include <iostream>
+#include <fstream>
+#include<dirent.h>
+
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+bool clipFlow = true;
+
+inline bool isFlowCorrect(Point2f u)
+{
+    return !cvIsNaN(u.x) && !cvIsNaN(u.y) && fabs(u.x) < 1e9 && fabs(u.y) < 1e9;
+}
+
+static Vec3b computeColor(float fx, float fy)
+{
+    static bool first = true;
+
+    // relative lengths of color transitions:
+    // these are chosen based on perceptual similarity
+    // (e.g. one can distinguish more shades between red and yellow
+    //  than between yellow and green)
+    const int RY = 15;
+    const int YG = 6;
+    const int GC = 4;
+    const int CB = 11;
+    const int BM = 13;
+    const int MR = 6;
+    const int NCOLS = RY + YG + GC + CB + BM + MR;
+    static Vec3i colorWheel[NCOLS];
+
+    if (first)
+    {
+        int k = 0;
+
+        for (int i = 0; i < RY; ++i, ++k)
+            colorWheel[k] = Vec3i(255, 255 * i / RY, 0);
+
+        for (int i = 0; i < YG; ++i, ++k)
+            colorWheel[k] = Vec3i(255 - 255 * i / YG, 255, 0);
+
+        for (int i = 0; i < GC; ++i, ++k)
+            colorWheel[k] = Vec3i(0, 255, 255 * i / GC);
+
+        for (int i = 0; i < CB; ++i, ++k)
+            colorWheel[k] = Vec3i(0, 255 - 255 * i / CB, 255);
+
+        for (int i = 0; i < BM; ++i, ++k)
+            colorWheel[k] = Vec3i(255 * i / BM, 0, 255);
+
+        for (int i = 0; i < MR; ++i, ++k)
+            colorWheel[k] = Vec3i(255, 0, 255 - 255 * i / MR);
+
+        first = false;
+    }
+
+    const float rad = sqrt(fx * fx + fy * fy);
+    const float a = atan2(-fy, -fx) / (float)CV_PI;
+
+    const float fk = (a + 1.0f) / 2.0f * (NCOLS - 1);
+    const int k0 = static_cast<int>(fk);
+    const int k1 = (k0 + 1) % NCOLS;
+    const float f = fk - k0;
+
+    Vec3b pix;
+
+    for (int b = 0; b < 3; b++)
+    {
+        const float col0 = colorWheel[k0][b] / 255.f;
+        const float col1 = colorWheel[k1][b] / 255.f;
+
+        float col = (1 - f) * col0 + f * col1;
+
+        if (rad <= 1)
+            col = 1 - rad * (1 - col); // increase saturation with radius
+        else
+            col *= .75; // out of range
+
+        pix[2 - b] = static_cast<uchar>(255.f * col);
+    }
+
+    return pix;
+}
+
+static void drawOpticalFlow(const Mat_<Point2f>& flow, Mat& dst, float maxmotion = -1)
+{
+    dst.create(flow.size(), CV_8UC3);
+    dst.setTo(Scalar::all(0));
+
+    // determine motion range:
+    float maxrad = maxmotion;
+
+    if (maxmotion <= 0)
+    {
+        maxrad = 1;
+        for (int y = 0; y < flow.rows; ++y)
+        {
+            for (int x = 0; x < flow.cols; ++x)
+            {
+                Point2f u = flow(y, x);
+
+                if (!isFlowCorrect(u))
+                    continue;
+
+                maxrad = max(maxrad, sqrt(u.x * u.x + u.y * u.y));
+            }
+        }
+    }
+
+    for (int y = 0; y < flow.rows; ++y)
+    {
+        for (int x = 0; x < flow.cols; ++x)
+        {
+            Point2f u = flow(y, x);
+
+            if (isFlowCorrect(u))
+                dst.at<Vec3b>(y, x) = computeColor(u.x / maxrad, u.y / maxrad);
+        }
+    }
+}
+
+// binary file format for flow data specified here:
+// http://vision.middlebury.edu/flow/data/
+static void writeOpticalFlowToFile(const Mat_<Point2f>& flow, const string& fileName)
+{
+    static const char FLO_TAG_STRING[] = "PIEH";
+
+    ofstream file(fileName.c_str(), ios_base::binary);
+
+    file << FLO_TAG_STRING;
+
+    file.write((const char*) &flow.cols, sizeof(int));
+    file.write((const char*) &flow.rows, sizeof(int));
+
+    for (int i = 0; i < flow.rows; ++i)
+    {
+        for (int j = 0; j < flow.cols; ++j)
+        {
+            const Point2f u = flow(i, j);
+
+            file.write((const char*) &u.x, sizeof(float));
+            file.write((const char*) &u.y, sizeof(float));
+        }
+    }
+}
+
+static void convertFlowToImage(const Mat &flow_x, Mat &img_x, double lowerBound, double higherBound)
+{
+    #define CAST(v, L, H) ((v) > (H) ? 255 : (v) < (L) ? 0 : cvRound(255*((v) - (L))/((H)-(L))))
+    for (int i = 0; i < flow_x.rows; ++i) {
+        for (int j = 0; j < flow_x.cols; ++j) {
+            float x = flow_x.at<float>(i,j);
+            img_x.at<uchar>(i,j) = CAST(x, lowerBound, higherBound);
+        }
+    }
+    #undef CAST
+}
+
+
+
+int main(int argc, const char* argv[])
+{
+  //  if (argc < 3)
+  //  {
+  //      cerr << "Usage : " << argv[0] << "<frame0> <frame1> [<output_flow>]" << endl;
+  //      return -1;
+  //  }
+
+    Mat image,prev_image;
+
+    string outputPath="/home/mcg/cxk/dataset/somthing-something/something-optical-flow/";
+    DIR* dir=opendir("/home/mcg/cxk/dataset/somthing-something/20bn-something-something-v1");
+    dirent* p=NULL;
+    while((p=readdir(dir))!=NULL)
+    {
+        if(p->d_name[0]!='.')
+        {
+            string name="/home/mcg/cxk/dataset/somthing-something/20bn-something-something-v1/"+string(p->d_name);
+            cout<<name<<endl;
+
+            int status;
+            string newPath=outputPath+string(p->d_name);
+            status=mkdir(newPath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+            if(status!=0)
+            {
+                cout<<"error in mkdir!"<<endl;
+                cout<<status<<endl;
+                cout<<newPath<<endl;
+                return -1;
+            }
+
+
+            int count=0;
+            DIR* dirChild=opendir(name.c_str());
+            dirent* pChild=NULL;
+            while((pChild=readdir(dirChild))!=NULL)
+            {
+                if(pChild->d_name[0]!='.')
+                {
+                    string nameChild=name+'/'+string(pChild->d_name);
+                    //cout<<nameChild<<endl;
+                    ++count;
+                }
+            }
+            closedir(dirChild);
+            
+            for(int i=1;i<=count;i+=2)
+            {
+                char tmp[20];
+                sprintf(tmp,"%05d.jpg",int(i));
+                string imgName=name+'/'+tmp;
+                if(i==1)
+                {
+                    prev_image = imread(imgName, IMREAD_GRAYSCALE);
+                    if(prev_image.empty())
+                    {
+                        cerr << "Can't open image ["  << imgName << "]" << endl;
+                        return -1;
+                    }
+
+                }
+                image=imread(imgName, IMREAD_GRAYSCALE);
+                if(image.empty())
+                {
+                    cerr << "Can't open image ["  << imgName << "]" << endl;
+                    return -1;
+                }
+                if (image.size() != prev_image.size())
+                {
+                    cerr << "Images should be of equal sizes" << endl;
+                    cerr<<image.size()<<prev_image.size()<<endl;
+                    return -1;
+                }
+
+    
+                Mat_<Point2f> flow;
+                Ptr<DenseOpticalFlow> tvl1 = createOptFlow_DualTVL1();
+
+                const double start = (double)getTickCount();
+                tvl1->calc(prev_image, image, flow);
+                const double timeSec = (getTickCount() - start) / getTickFrequency();
+                cout << "calcOpticalFlowDual_TVL1 : " << timeSec << " sec" << endl;
+
+
+                Mat x(flow.rows,flow.cols,CV_64FC1);
+                Mat y(flow.rows,flow.cols,CV_64FC1);
+
+                Mat imgX(flow.rows,flow.cols,CV_8UC1);
+                Mat imgY(flow.rows,flow.cols,CV_8UC1);
+
+
+                float* px=x.ptr<float>(0);
+                float* py=y.ptr<float>(0);
+
+                for (int i = 0; i < flow.rows; ++i)
+                {
+                    px=x.ptr<float>(i);
+                    py=y.ptr<float>(i);
+                    for (int j = 0; j < flow.cols; ++j)
+                    {
+                        const Point2f u = flow(i, j);
+                        px[j]=u.x;
+                        py[j]=u.y;
+                    }
+                }
+
+                double min_x,max_x;
+                double min_y,max_y;
+                minMaxLoc(x,&min_x,&min_x);
+                minMaxLoc(y,&min_y,&min_y);
+
+                if(clipFlow)
+                {
+                    min_x=-20; max_x=20;
+                    min_y=-20; max_y=20;
+                }
+                convertFlowToImage(x,imgX,min_x,max_x);
+                convertFlowToImage(y,imgY,min_y,max_y);
+
+                imwrite(outputPath+string(p->d_name)+'/'+"x_"+tmp,imgX);
+                imwrite(outputPath+string(p->d_name)+'/'+"y_"+tmp,imgY);
+
+                std::swap(prev_image,image);
+                           
+            }
+
+        }
+    }
+    closedir(dir);
+
+//    Mat out;
+//    drawOpticalFlow(flow, out);
+//
+//    if (argc == 4)
+//        writeOpticalFlowToFile(flow, argv[3]);
+//
+//    imwrite("Flow.jpg", out);
+//    cout<<"ok"<<endl;
+//
+//    waitKey();
+
+    return 0;
+}
